3cbf8ec792fc8c609fd68b9d4465504f859eb9e4
datocarneiro <132966071+datocarneiro@users.noreply.github.com>
2023-12-26 18:09:55 -0300

    Update Documentação_Python.py

--- Básico/Documentação_Python.py
+++ Básico/Documentação_Python.py
@@ -1,10 +1,8 @@
 """
+Link para documentaçã o Python
+
 https://docs.python.org/pt-br/3/library/stdtypes.html
-Imutáveis que vimos: str, int, float, bool
+
 """
-variavel = 'Dato'
-# outra_variavel = f'{string[:3]}ABC{string[4:]}'
-# print(string)
-# print(outra_variavel)
-print(variavel.zfill(10)) # 
-print(variavel.z) # 
+
+
