9578930ba9b2e859a2cf1cc8aaad7dd60c3af0f8
datocarneiro <datocarneiro@outlook.com>
2024-01-03 16:44:29 -0300

    Estudando

--- cpf.py
+++ cpf.py
@@ -23,20 +23,42 @@
 
 #     O primeiro dígito do CPF é 7
 
+# 75532282064
+# 602.440.980-00 , 755.322.820-64
+def formatar_cpf(cpf_formatado):
+    # Formatar o CPF no padrão xxx.xxx.xxx-xx
+    cpf_digitado = str(cpf_input)
+    cpf_formatado = f'{cpf_digitado[:9]}-{cpf_digitado[9:]}'
+    
+    return cpf_formatado
 
+# Solicitar ao usuário que digite o CPF
+while True:    
+    try:
+        cpf_input = 74682489070 # int(input("Digite o CPF [apenas números]: "))
+        # Formatando o CPF e exibindo o resultado
+        cpf_formatado = formatar_cpf(cpf_input)
+        print(f"CPF informado: {cpf_formatado}")
+        break
+    except ValueError:
+        print('Digite apenas Números Inteiros:')
 
-while True:
-    cpf = input('digite o CPF: xxxxxxxxx-xx: ')
-    print(cpf)
-    # try: 
-    #     print('Você digitou:',cpf)
-    # except ValueError:
-    #     print('Digite apenas números inteiros...')
+cpf_lista = []
+for i in cpf_formatado[:9]:
+    cpf_lista.append(i)
+print(cpf_lista)
 
-# cpf_tratado = []
-# # salvando cada numero do cpf, em indeices separados
-# for i in cpf:
-#     cpf_tratado.append(i)
-# print(cpf_tratado)
-
-# #
+#conta multiplicação 
+resultado = []
+numero_para_multiplicar = 10
+for i in cpf_lista:
+    i = int(i) * numero_para_multiplicar
+    numero_para_multiplicar -= 1
+    resultado.append(i)
+print(resultado)
+soma = sum(resultado)
+print(soma)
+resultado_mul2 = soma * 10
+print(f'Multiplicar o resultado anterior por 10:......{resultado_mul2}')
+resto = resultado_mul2 % 11
+print(f'Obter o resto da divisão da conta anterior por 11:....... {resto}')
