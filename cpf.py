3f5e2c1e20f4977dfd4e3fbbb814d7f7ba92a243
datocarneiro <datocarneiro@outlook.com>
2024-01-04 00:39:28 -0300

    Estudando

--- cpf.py
+++ cpf.py
@@ -23,11 +23,10 @@
 
 #     O primeiro dígito do CPF é 7
 
-# 75532282064
-# 602.440.980-00 , 755.322.820-64
 def formatar_cpf(cpf_formatado):
     # Formatar o CPF no padrão xxx.xxx.xxx-xx
     cpf_digitado = str(cpf_input)
+    # cpf_formatado = f'{cpf_digitado[:9]}-{cpf_digitado[9:]}'
     cpf_formatado = f'{cpf_digitado[:9]}-{cpf_digitado[9:]}'
     
     return cpf_formatado
@@ -35,30 +34,72 @@ def formatar_cpf(cpf_formatado):
 # Solicitar ao usuário que digite o CPF
 while True:    
     try:
-        cpf_input = 74682489070 # int(input("Digite o CPF [apenas números]: "))
+        cpf_input = int(input("Digite o CPF [apenas números]: "))
         # Formatando o CPF e exibindo o resultado
         cpf_formatado = formatar_cpf(cpf_input)
-        print(f"CPF informado: {cpf_formatado}")
+        print(f"CPF informado:... {cpf_formatado}")
         break
     except ValueError:
         print('Digite apenas Números Inteiros:')
 
-cpf_lista = []
-for i in cpf_formatado[:9]:
-    cpf_lista.append(i)
-print(cpf_lista)
+#nove_digitos = cpf_formatado[:9]
+nove_digitos = str(cpf_input)
 
-#conta multiplicação 
-resultado = []
-numero_para_multiplicar = 10
-for i in cpf_lista:
-    i = int(i) * numero_para_multiplicar
-    numero_para_multiplicar -= 1
-    resultado.append(i)
-print(resultado)
-soma = sum(resultado)
-print(soma)
-resultado_mul2 = soma * 10
-print(f'Multiplicar o resultado anterior por 10:......{resultado_mul2}')
-resto = resultado_mul2 % 11
-print(f'Obter o resto da divisão da conta anterior por 11:....... {resto}')
+
+
+resultado = 0
+numero_regressivo1 = 10
+for i in nove_digitos:
+    resultado += int(i) * numero_regressivo1
+    numero_regressivo1 -= 1
+
+primeiro_digito = (resultado * 10) % 11 if (resultado * 10) % 11 <= 9 else 0
+print(f'Validando o primeiro dígito:... {primeiro_digito}')
+
+#############################################################################################################
+
+# Calculo do segundo dígito do CPF
+# CPF: 746.824.890-70
+# Colete a soma dos 9 primeiros dígitos do CPF,
+# MAIS O PRIMEIRO DIGITO,
+# multiplicando cada um dos valores por uma
+# contagem regressiva começando de 11
+
+# Ex.:  746.824.890-70 (7468248907)
+#    11 10  9  8  7  6  5  4  3  2
+# *  7   4  6  8  2  4  8  9  0  7 <-- PRIMEIRO DIGITO
+#    77 40 54 64 14 24 40 36  0 14
+
+# Somar todos os resultados:
+# 77+40+54+64+14+24+40+36+0+14 = 363
+# Multiplicar o resultado anterior por 10
+# 363 * 10 = 3630
+# Obter o resto da divisão da conta anterior por 11
+# 3630 % 11 = 0
+# Se o resultado anterior for maior que 9:
+#     resultado é 0
+# contrário disso:
+#     resultado é o valor da conta
+
+# O segundo dígito do CPF é 0
+
+#concatenando os 9 digitos + o primeiro dígito
+dez_digitos = nove_digitos+str(primeiro_digito)
+print(f'Dez dígitos:... {dez_digitos}')
+
+resultado = 0
+numero_regressivo2 = 11
+for i in dez_digitos:
+    resultado += int(i) * numero_regressivo2
+    numero_regressivo2 -= 1
+
+segundo_digito = (resultado * 10) % 11 if (resultado * 10) % 11 <= 9 else 0
+print(f'Validando o segundo dígito:... {segundo_digito}')
+
+resultado_cpf = dez_digitos + str(segundo_digito)
+# resultado_cpf_formatado = formatar_cpf(resultado_cpf)
+cpf_formatado = f'{resultado_cpf[:9]}-{resultado_cpf[9:]}'
+print(cpf_formatado)
+
+if 
+
