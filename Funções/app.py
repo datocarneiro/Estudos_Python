e2624e868d22e020db0b71fe0bf9b880ae312207
datocarneiro <datocarneiro@outlook.com>
2024-01-07 03:53:20 -0300

    Estudando

--- Funções/app.py
+++ Funções/app.py
@@ -1,17 +1,20 @@
-x = 300
-def local1():
-    x=10
-    print(f'x local 1 = {x}')
-    def local2():
-        x=20
-        print(f'x local 2 = {x}')
-    # chamando a função local2
-    local2()
-# x nesse caso ESTA acessivel fora das funções
-print(f'x global = {x}') 
-# chamando a função local 1
-local1()
-print(f'x global apos função = {x}') 
-######################################################################
-# Resultado:  x local 1 = 10
-#             x local 2 = 100
+# Exercícios com funções
+
+# Crie uma função que multiplica todos os argumentos não nomeados recebidos
+# Retorne o total para uma variável e mostre o valor da variável.
+
+# crie uma função fala se um numero é par ou impar 
+# retorna se o numerp é par ou impar
+
+def calculo(*args):
+    global total
+    total= 0
+    for i in args:
+        total *= i
+        print(total)
+    return total
+
+print(1*2*3)
+
+calculando = calculo(*1,2,3)
+print(calculando)
