6654d3a5859c8c8a775b6322366876247b97dfdf
datocarneiro <datocarneiro@outlook.com>
2024-01-11 00:26:57 -0300

    Estudando

---
+++ Dicionarios/app.py
@@ -0,0 +1,10 @@
+pessoa = {
+    'nome': 'Dato',
+    'sobrenome': 'Carneiro',
+    'idade': 18,
+}
+for i in pessoa.values():   
+    print(i)
+# Resultado:    Dato
+#               Carneiro
+#               18
